933f19ee05b4edc60399bc378190bcf00d64dc3b
Zharkov Andrey <zharkov.php@gmail.com>
2017-09-03 13:01:15 +0300

    Install and Load Bootstrap 4.0 Template in my project

--- views/site/index.php
+++ views/site/index.php
@@ -1,6 +1,34 @@
 <?php include ROOT . '/views/layouts/header.php'; ?>
+    
+    <div class="container-fluid">
 
-Site/index;<br>
-<hr>
+                <table class="table table-hover">
+                        <thead>
+                        <tr>
+                                <th>Фото</th>
+                                <th>Имя</th>
+                                <th>Email</th>
+                                <th>Задача</th>
+                                <th>Статус</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                                <td><img src="/template/images/19621036_239521669887890_1208747178532153914_o.jpg" class="img-thumbnail" alt="Cinque Terre" width="100" height="100"></td>
+                                <td> 1<br></td>
+                                <td> 2<br></td>
+                                <td> 3<br></td>
+                                <td> 4<button type="button" class="btn btn-primary"><a href="#">
+                                            <font color="white">...</font>
+                                        </a></button<br></td>
 
-<?php include ROOT . '/views/layouts/header.php'; ?>
+                            </tr>
+            
+                        </tbody>
+                    </table>
+
+    </div>
+    
+
+    <?php include ROOT . '/views/layouts/footer.php'; ?>
